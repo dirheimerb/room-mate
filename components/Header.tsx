cc58947aa54fe96c8c1ea6b9c7501f9648d92fc2
Bradley Dirheimer <dirheimerb@gmail.com>
2023-03-06 13:45:46 -0500

    h1

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
           height={24}
         />
         <h1 className="sm:text-3xl text-xl font-bold ml-2 tracking-tight">
-          RoomMate
+          RooMate
         </h1>
       </Link>
       <a
