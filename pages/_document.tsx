7c71020f5677a94f9c104c947002b5e0c7256f49
Bradley Dirheimer <dirheimerb@gmail.com>
2023-03-06 13:41:37 -0500

    Homepage

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,8 @@ class MyDocument extends Document {
   render() {
     let description = 'Generate your dream room in seconds.';
     let ogimage = 'https://www.roomgpt.io/og-image.png';
-    let sitename = 'roomGPT.io';
-    let title = 'Dream Room Generator';
+    let sitename = 'roomMate';
+    let title = 'RoomMate';
 
     return (
       <Html lang="en">
